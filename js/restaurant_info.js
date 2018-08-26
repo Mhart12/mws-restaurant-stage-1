478d438ec7fcec9ae23b065255a7f6e6d502b250
Monica Hart <mhart2012@gmail.com>
2018-08-26 11:25:58 -0500

    Req. changes, image.alt to main.js and restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,7 +88,9 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.alt = '${restaurant.name} image';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  li.append(image);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
